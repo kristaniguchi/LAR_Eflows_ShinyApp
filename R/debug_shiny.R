56b53b6fb9926076bc2375923e0155302ed290f4
Jose Toledo <toledojose60@gmail.com>
2021-04-01 09:42:41 -0700

    fixed issue with winter peak flows not showing up

--- R/debug_shiny.R
+++ R/debug_shiny.R
@@ -52,16 +52,17 @@ df2 <- dat %>% filter(Node=="GLEN",
                Designation_Category == "Existing"| is.na(Designation_Category),
                BU_names %in% dat$BU_names[grep("WILD",dat$BU_names)]|is.na(BU_names),
                metric %in% c("DS_Mag_50","Wet_BFL_Mag_10",
-                             "Peak_2 as lower, 
-                             Peak_10 as upper")|is.na(metric),
+                             "Peak_2 as lower, Peak_10 as upper")|is.na(metric),
                Probability_Threshold=="Medium"|is.na(Probability_Threshold)) 
 
 
 df3 <- dat %>% filter(Node=="GLEN",
-                      Designation=="Existing"|is.na(Designation),
-                      Probability_Threshold=="Medium"|is.na(Probability_Threshold),
-                      metric %in% c("DS_Mag_50","Wet_BFL_Mag_10","Peak_2 as lower, Peak_10 as upper")|is.na(metric)) %>%
-  mutate(Range = paste0(Lower_Limit,"-",Upper_Limit))
+                      Designation_BU %in% "Designated"| is.na(Designation_BU),
+                      Designation_Category %in% "Existing"| is.na(Designation_Category),
+                      BU_names %in% dat$BU_names,
+                      metric %in% c("DS_Mag_50","Wet_BFL_Mag_10",
+                                    "Peak_2 as lower, Peak_10 as upper")|is.na(metric),
+                      Probability_Threshold %in% "Medium"|is.na(Probability_Threshold)) 
 
 
 ggplot(df2,aes(x=Species_Label, ymin = Lower_Limit,
@@ -89,24 +90,36 @@ ggplot(df2,aes(x=Species_Label, ymin = Lower_Limit,
 # Synthesis Rule Set ------------------------------------------------------
 
 
-# individual species
+# multiple species by season
 
-df2 %>% filter(Species != "Current Flow") %>%
-  group_by(Seasonal_Component,Species_Synthesis) %>%
-  select(Species_Synthesis,Lower_Limit,Upper_Limit)  %>%
-  summarise(a=min(Lower_Limit),b=min(Upper_Limit))
+synthesis <- df2  %>%
+  group_by(Seasonal_Component,Species) %>%
+  select(Species,Lower_Limit,Upper_Limit)  %>%
+  summarise(a=min(Lower_Limit),b=min(Upper_Limit)) %>% 
+  group_split()
+
+synthesis_a = lapply(synthesis, function(x) x %>% select(a))
+synthesis_b = lapply(synthesis, function(x) x %>% select(b))
 
 
+synthesis_a_range = lapply(synthesis_a, range)
+synthesis_b_range = lapply(synthesis_b, range)
 
 
-# multiple species
+seq_funcion <- function(x){
+  c(range(x)[1]:range(x)[2])
+}
 
+synthesis_a_seq <- lapply(synthesis_a,seq_funcion)
+synthesis_b_seq <- lapply(synthesis_b,seq_funcion)
 
-df2 %>% filter(Species != "Current Flow") %>%
-  group_by(Seasonal_Component) %>%
-  select(Species_Synthesis,Lower_Limit,Upper_Limit)  %>%
-  summarise(a=min(Lower_Limit),b=min(Upper_Limit))
+synthesis_multiple_species = list()
+for(i in 1:length(synthesis_a_seq)){
+  synthesis_multiple_species[[i]] = Reduce(intersect,
+                                           list(synthesis_a_seq[[i]],
+                                                synthesis_b_seq[[i]]))
 
+}
 
 
 
